193efaed029c6ed3cac6189cd6e9ca88f71939d6
OOPer <nagata@shlab.jp>
2017-05-09 21:33:09 +0900

    Updated base sample code to Version 6.0, 2017-03-21

---
+++ README2.md
@@ -0,0 +1,25 @@
+# Application Icons and Launch Images for iOS
+
+Translated by OOPer in cooperation with shlab.jp, on 2014/12/06.
+
+Based on
+<https://developer.apple.com/library/content/samplecode/Icons/Introduction/Intro.html#//apple_ref/doc/uid/DTS40010442>
+2017-03-27.
+
+As this is a line-by-line translation from the original sample code, "redistribute the Apple Software in its entirety and without modifications" would apply. See license terms in each file .
+Some faults caused by my translation may exist. Not all features tested.
+You should not contact to Apple or SHLab(jp) about any faults caused by my translation.
+
+================================================================================
+BUILD REQUIREMENTS:
+
+Xcode 8.3.2, iOS SDK 10
+
+
+================================================================================
+RUNTIME REQUIREMENTS:
+
+iOS 8.0 or later
+
+
+================================================================================
--- Shared/RootViewController.m
+++ Shared/RootViewController.m
@@ -1,48 +1,9 @@
 /*
-     File: RootViewController.m
- Abstract: The view controller displays what each icon does on iOS.
-  Version: 1.3
+ Copyright (C) 2017 Apple Inc. All Rights Reserved.
+ See LICENSE.txt for this sample’s licensing information
  
- Disclaimer: IMPORTANT:  This Apple software is supplied to you by Apple
- Inc. ("Apple") in consideration of your agreement to the following
- terms, and your use, installation, modification or redistribution of
- this Apple software constitutes acceptance of these terms.  If you do
- not agree with these terms, please do not use, install, modify or
- redistribute this Apple software.
- 
- In consideration of your agreement to abide by the following terms, and
- subject to these terms, Apple grants you a personal, non-exclusive
- license, under Apple's copyrights in this original Apple software (the
- "Apple Software"), to use, reproduce, modify and redistribute the Apple
- Software, with or without modifications, in source and/or binary forms;
- provided that if you redistribute the Apple Software in its entirety and
- without modifications, you must retain this notice and the following
- text and disclaimers in all such redistributions of the Apple Software.
- Neither the name, trademarks, service marks or logos of Apple Inc. may
- be used to endorse or promote products derived from the Apple Software
- without specific prior written permission from Apple.  Except as
- expressly stated in this notice, no other rights or licenses, express or
- implied, are granted by Apple herein, including but not limited to any
- patent rights that may be infringed by your derivative works or by other
- works in which the Apple Software may be incorporated.
- 
- The Apple Software is provided by Apple on an "AS IS" basis.  APPLE
- MAKES NO WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
- THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS
- FOR A PARTICULAR PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS USE AND
- OPERATION ALONE OR IN COMBINATION WITH YOUR PRODUCTS.
- 
- IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL
- OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE, REPRODUCTION,
- MODIFICATION AND/OR DISTRIBUTION OF THE APPLE SOFTWARE, HOWEVER CAUSED
- AND WHETHER UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE),
- STRICT LIABILITY OR OTHERWISE, EVEN IF APPLE HAS BEEN ADVISED OF THE
- POSSIBILITY OF SUCH DAMAGE.
- 
- Copyright (C) 2014 Apple Inc. All Rights Reserved.
- 
+ Abstract:
+ The view controller displays what each icon does on iOS.
  */
 
 #import "RootViewController.h"
@@ -68,70 +29,74 @@ NSString * const kIconCellHeight = @"kIconCellHeight";
     [super viewDidLoad];
     
     self.icons = @[
-                   @{kIconName: @"Icon-60@3x",
-                     kIconDescription: @"Home screen on iPhone 6 Plus",
-                     kIconCellHeight: @(60)},
-                   
-                   @{kIconName: @"Icon-60@2x",
-                     kIconDescription: @"Home screen on iPhone/iPod Touch with retina display (iOS 7+)",
-                     kIconCellHeight: @(60)},
-                   
-                   @{kIconName: @"Icon-76",
-                     kIconDescription: @"Home screen on iPad (iOS 7)",
-                     kIconCellHeight: @(76)},
-                   
-                   @{kIconName: @"Icon-76@2x",
-                     kIconDescription: @"Home screen on iPad with retina display (iOS 7+)",
-                     kIconCellHeight: @(76)},
-                   
-                   @{kIconName: @"Icon-Small-40",
-                     kIconDescription: @"Spotlight (iOS 7)",
-                     kIconCellHeight: @(40)},
-                   
-                   @{kIconName: @"Icon-Small-40@2x",
-                     kIconDescription: @"Spotlight on devices with retina display (iOS 7+)",
-                     kIconCellHeight: @(40)},
-                   
-                   @{kIconName: @"Icon-Small-40@3x",
-                     kIconDescription: @"Spotlight on iPhone 6 Plus",
-                     kIconCellHeight: @(40)},
-                   
-                   @{kIconName: @"Icon-Small",
-                     kIconDescription: @"Spotlight on iPhone/iPod Touch (iOS 6.1 and earlier) and Settings",
-                     kIconCellHeight: @(40)},
-                   
-                   @{kIconName: @"Icon-Small@2x",
-                     kIconDescription: @"Spotlight on iPhone/iPod Touch with retina display (iOS 6.1 and earlier) and Settings on devices with retina display",
-                     kIconCellHeight: @(75)},
-                   
-                   @{kIconName: @"Icon-Small@3x",
-                     kIconDescription: @"Settings on iPhone 6 Plus",
-                     kIconCellHeight: @(40)},
-                   
-                   @{kIconName: @"Icon",
-                     kIconDescription: @"Home screen on iPhone/iPod touch (iOS 6.1 and earlier)",
-                     kIconCellHeight: @(57)},
-                   
-                   @{kIconName: @"Icon@2x",
-                     kIconDescription: @"Home screen on iPhone/iPod Touch with retina display (iOS 6.1 and earlier)",
-                     kIconCellHeight: @(57)},
-                   
-                   @{kIconName: @"Icon-72",
-                     kIconDescription: @"Home screen on iPad (iOS 6.1 and earlier)",
-                     kIconCellHeight: @(72)},
-                   
-                   @{kIconName: @"Icon-72@2x",
-                     kIconDescription: @"Home screen on iPad with retina display (iOS 6.1 and earlier)",
-                     kIconCellHeight: @(72)},
-                   
-                   @{kIconName: @"Icon-Small-50",
-                     kIconDescription: @"Spotlight on iPad (iOS 6.1 and earlier)",
-                     kIconCellHeight: @(50)},
-                   
-                   @{kIconName: @"Icon-Small-50@2x",
-                     kIconDescription: @"Spotlight on iPad with retina display (iOS 6.1 and earlier)",
-                     kIconCellHeight: @(50)},
-                   ];
+       @{kIconName: @"Icon-60@3x",
+         kIconDescription: @"Home screen on iPhone with retina HD display",
+         kIconCellHeight: @(60)},
+       
+       @{kIconName: @"Icon-60@2x",
+         kIconDescription: @"Home screen on iPhone/iPod Touch with retina display (iOS 7+)",
+         kIconCellHeight: @(60)},
+       
+       @{kIconName: @"Icon-76",
+         kIconDescription: @"Home screen on iPad (iOS 7+)",
+         kIconCellHeight: @(76)},
+       
+       @{kIconName: @"Icon-76@2x",
+         kIconDescription: @"Home screen on iPad with retina display (iOS 7+)",
+         kIconCellHeight: @(76)},
+       
+       @{kIconName: @"Icon-83.5@2x",
+         kIconDescription: @"Home screen on iPad Pro",
+         kIconCellHeight: @(83.5)},
+       
+       @{kIconName: @"Icon-Small-40",
+         kIconDescription: @"Spotlight (iOS 7+)",
+         kIconCellHeight: @(40)},
+       
+       @{kIconName: @"Icon-Small-40@2x",
+         kIconDescription: @"Spotlight on devices with retina display (iOS 7+)",
+         kIconCellHeight: @(40)},
+       
+       @{kIconName: @"Icon-Small-40@3x",
+         kIconDescription: @"Spotlight on devices with retina HD display",
+         kIconCellHeight: @(40)},
+       
+       @{kIconName: @"Icon-Small",
+         kIconDescription: @"Spotlight on iPhone/iPod Touch (iOS 6.1 and earlier), and Settings on all devices",
+         kIconCellHeight: @(40)},
+       
+       @{kIconName: @"Icon-Small@2x",
+         kIconDescription: @"Spotlight on iPhone/iPod Touch with retina display (iOS 6.1 and earlier), and Settings on all devices with retina display",
+         kIconCellHeight: @(40)},
+       
+       @{kIconName: @"Icon-Small@3x",
+         kIconDescription: @"Settings on devices with retina HD display",
+         kIconCellHeight: @(40)},
+       
+       @{kIconName: @"Icon",
+         kIconDescription: @"Home screen on iPhone/iPod touch (iOS 6.1 and earlier)",
+         kIconCellHeight: @(57)},
+       
+       @{kIconName: @"Icon@2x",
+         kIconDescription: @"Home screen on iPhone/iPod Touch with retina display (iOS 6.1 and earlier)",
+         kIconCellHeight: @(57)},
+       
+       @{kIconName: @"Icon-72",
+         kIconDescription: @"Home screen on iPad (iOS 6.1 and earlier)",
+         kIconCellHeight: @(72)},
+       
+       @{kIconName: @"Icon-72@2x",
+         kIconDescription: @"Home screen on iPad with retina display (iOS 6.1 and earlier)",
+         kIconCellHeight: @(72)},
+       
+       @{kIconName: @"Icon-Small-50",
+         kIconDescription: @"Spotlight on iPad (iOS 6.1 and earlier)",
+         kIconCellHeight: @(50)},
+       
+       @{kIconName: @"Icon-Small-50@2x",
+         kIconDescription: @"Spotlight on iPad with retina display (iOS 6.1 and earlier)",
+         kIconCellHeight: @(50)},
+    ];
 }
 
 #pragma mark -
@@ -164,8 +129,9 @@ NSString * const kIconCellHeight = @"kIconCellHeight";
     NSString *iconName = self.icons[indexPath.row][kIconName];
     NSString *iconPath = [[NSBundle mainBundle] pathForResource:iconName ofType:@"png"];
     BOOL isRetina = [iconName rangeOfString:@"@2x"].location != NSNotFound;
+    BOOL isRetinaHD = [iconName rangeOfString:@"@3x"].location != NSNotFound;
     
-    cell.imageView.image = [[UIImage alloc] initWithData:[NSData dataWithContentsOfFile:iconPath] scale:(isRetina ? 2 : 1)];
+    cell.imageView.image = [[UIImage alloc] initWithData:[NSData dataWithContentsOfFile:iconPath] scale:(isRetinaHD ? 3 : (isRetina ? 2 : 1))];
     cell.textLabel.text = [iconName stringByAppendingString:@".png"];
 	cell.detailTextLabel.text = self.icons[indexPath.row][kIconDescription];
     
